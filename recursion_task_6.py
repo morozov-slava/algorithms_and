66138a00e15878db63a9b7e7fefd5a6c401238b3
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-09-25 20:58:47 +0500

    Update recursion_task_6.py

--- recursion_task_6.py
+++ recursion_task_6.py
@@ -1,7 +1,10 @@
 # 6. Печать элементов списка с чётными индексами
-def print_even_indices_elements(array: list, index=0):
+def print_even_indices_elements(array: list, index: int):
     if index < len(array):
         print(array[index])
         print_even_indices_elements(array, index+2)
 
+START_INDEX = 0
+array = [1, 2, 3, 4, 5]
+print_even_indices_elements(array, START_INDEX)
 
