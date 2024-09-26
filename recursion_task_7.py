0403571078b68a16661491adedc62fbd8d658c24
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-09-26 20:30:34 +0500

    Update recursion_task_7.py

--- recursion_task_7.py
+++ recursion_task_7.py
@@ -1,15 +1,14 @@
+def find_second_max_number(max_number: int, second_max_number: int, index: int):
+    if index >= len(array):
+        return second_max_number
+    if array[index] > max_number:
+        second_max_number = max_number
+        max_number = array[index]
+    elif array[index] == max_number:
+        second_max_number = max_number
+    elif array[index] > second_max_number:
+        second_max_number = array[index]
+    return find_second_max_number(max_number, second_max_number, index+1)
+
 def main(array: list):
-    def find_second_max_number(max_number: int, second_max_number: int, index: int):
-        if index >= len(array):
-            return second_max_number
-        if array[index] > max_number:
-            second_max_number = max_number
-            max_number = array[index]
-        elif array[index] == max_number:
-            second_max_number = max_number
-        elif array[index] > second_max_number:
-            second_max_number = array[index]
-        return recursive_second_max_number(max_number, second_max_number, index+1)
     return find_second_max_number(array[0], array[0], 0)
-
-
