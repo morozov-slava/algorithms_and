20246d60926f3db7e2141f12817f950141696012
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-09-27 23:03:15 +0500

    Update recursion_task_6.py

--- recursion_task_6.py
+++ recursion_task_6.py
@@ -2,9 +2,7 @@
 def print_even_indices_elements(array: list, index: int):
     if index >= len(array):
         return None
-    if index % 2 == 0:
-        print(array[index])
-    print_even_indices_elements(array, index+1)
+    print_even_indices_elements(array, index+2)
 
 def main(array: list):
     return print_even_indices_elements(array, 0)
