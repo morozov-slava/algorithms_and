e41b4ce2eeef2797cb2b6e62f15b33e5e88a0058
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-09-28 15:50:45 +0500

    Update recursion_palindrome.py

--- recursion_palindrome.py
+++ recursion_palindrome.py
@@ -2,7 +2,6 @@
 def is_palindrome_string(string: str, index: int):
     if index >= len(string) // 2:
         return True
-    print(f"{string[index]} --- {string[-1*index-1]}") 
     if not string[index] == string[-1*index-1]:
         return False
     return is_palindrome_string(string, index+1)
