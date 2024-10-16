44b5f5a1d24b7c7fc3b8060c87b62313a99d94b6
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-10-16 22:58:23 +0500

    Update balanced_bst.py

--- balanced_bst.py
+++ balanced_bst.py
@@ -1,19 +1,15 @@
-def get_balanced_bst(array: list):
-    if len(array) == 0:
+def get_balanced_bst(arr: list, start: int, end: int):
+    if start > end:
         return []
     balanced_bst = []
-    root_index = len(array) // 2
-    balanced_bst.append(array[root_index])
-    balanced_bst.extend(
-        get_balanced_bst(array[:root_index])
-    )
-    balanced_bst.extend(
-        get_balanced_bst(array[root_index+1:])
-    )
+    mid = start + (end - start) // 2
+    balanced_bst.append(arr[mid])
+    balanced_bst = get_balanced_bst(arr, start, mid - 1) + balanced_bst
+    balanced_bst = balanced_bst + get_balanced_bst(arr, mid + 1, end) 
     return balanced_bst
 
 def GenerateBBSTArray(a: list):
     a = sorted(a)
-    return get_balanced_bst(a)
+    return get_balanced_bst(a, 0, len(a)-1)
 
 
