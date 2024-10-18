edd61b39c4b58f023b14874a514e81a33dc54525
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-10-18 23:01:43 +0500

    Update balanced_bst.py

--- balanced_bst.py
+++ balanced_bst.py
@@ -1,15 +1,24 @@
-def get_balanced_bst(arr: list, start: int, end: int):
-    if start > end:
-        return []
-    balanced_bst = []
-    mid = start + (end - start) // 2
-    balanced_bst.append(arr[mid])
-    balanced_bst = get_balanced_bst(arr, start, mid - 1) + balanced_bst
-    balanced_bst = balanced_bst + get_balanced_bst(arr, mid + 1, end) 
-    return balanced_bst
+def get_balanced_bst(one_level_arrays: list):
+    balanced_bst_array = []
+    next_level_arrays = []
+    for array in one_level_arrays:
+        if len(array):
+            mid_i = len(array) // 2
+            balanced_bst_array.append(array[mid_i])
+        if len(array) > 2: 
+            next_level_arrays.append(array[:mid_i])
+            next_level_arrays.append(array[mid_i+1:])
+        elif len(array) == 2:
+            next_level_arrays.append(array[:mid_i])
+    if len(next_level_arrays):
+        balanced_bst_array.extend(
+            get_balanced_bst(next_level_arrays)
+        )
+    return balanced_bst_array
+    
 
 def GenerateBBSTArray(a: list):
     a = sorted(a)
-    return get_balanced_bst(a, 0, len(a)-1)
+    return get_balanced_bst([a])
 
 
