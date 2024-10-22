16fb7802534b40fa67d3168a2c436387ab1d7096
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-10-22 12:54:54 +0500

    Update balanced_bst_2.py

--- balanced_bst_2.py
+++ balanced_bst_2.py
@@ -22,14 +22,27 @@ class BalancedBST:
         if len(array) > 2:
             RootNode.RightChild = self._get_balanced_bst(RootNode, array[mid_i+1:], current_level+1)
         return RootNode
-        
+
+    def _is_balanced_subtree(self, Node: BSTNode, current_level: int):
+        if Node is None:
+            return current_level
+        left_max_level = self._is_balanced_subtree(Node.LeftChild, current_level+1)
+        right_max_level = self._is_balanced_subtree(Node.RightChild, current_level+1)
+        if abs(left_max_level - right_max_level) > 1: 
+            return False
+        return max(left_max_level, right_max_level)
+
     def GenerateTree(self, a: list):
         if len(a) == 0:
             self.Root = None
         a = sorted(a)
         self._get_balanced_bst(None, a, 0)
 
-    def IsBalanced(self, root_node):
-        return False # сбалансировано ли дерево с корнем root_node
+    def IsBalanced(self, root_node: BSTNode):
+        if root_node is None:
+            return True
+        if self._is_balanced_subtree(root_node, 0):
+            return True
+        return False
 
 
