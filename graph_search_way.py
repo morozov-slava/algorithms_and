2dc0ea194d6db19dcd330749fd1d7be0d7c267f6
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-11-06 14:29:42 +0500

    Create graph_search_way.py

---
+++ graph_search_way.py
@@ -0,0 +1,70 @@
+class Vertex:
+    def __init__(self, val):
+        self.Value = val
+        self.Hit = False
+  
+class SimpleGraph:
+    def __init__(self, size: int):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex = [None] * size
+        
+    def AddVertex(self, v: int):
+        empty_index = None
+        for i in range(len(self.vertex)):
+            if self.vertex[i] is None:
+                empty_index = i
+                break
+        if empty_index is not None:
+            self.vertex[empty_index] = Vertex(v)
+
+    def RemoveVertex(self, v: int):
+        if self.vertex[v] is not None:
+            self.vertex[v] = None
+        # remove edges
+        for i in range(self.max_vertex):
+            if self.m_adjacency[v][i] != 0:
+                self.m_adjacency[v][i] = 0
+            if self.m_adjacency[i][v] != 0:
+                self.m_adjacency[i][v] = 0 
+	
+    def IsEdge(self, v1: int, v2: int):
+        if (self.m_adjacency[v1] is None) or (self.m_adjacency[v2] is None):
+            return False
+        return bool(self.m_adjacency[v1][v2]) and bool(self.m_adjacency[v2][v1])
+	
+    def AddEdge(self, v1: int, v2: int):
+        if (self.m_adjacency[v1] is not None) or (self.m_adjacency[v2] is not None):
+            self.m_adjacency[v1][v2] = 1
+            self.m_adjacency[v2][v1] = 1
+	
+    def RemoveEdge(self, v1: int, v2: int):
+        if (self.m_adjacency[v1] is not None) and (self.m_adjacency[v2] is not None):
+            self.m_adjacency[v1][v2] = 0
+            self.m_adjacency[v2][v1] = 0
+
+    def DepthFirstSearch(self, VFrom: int, VTo: int):
+        for Node in self.vertex:
+            if (Node is None) or (Node.Hit == False):
+                continue
+            Node.Hit = False
+        return self._depth_first_search(VFrom, VTo)
+
+    def _depth_first_search(self, VFrom: int, VTo: int):
+        self.vertex[VFrom].Hit = True
+        path = [VFrom]
+        for i in range(self.max_vertex):
+            if self.vertex[i] is None:
+                continue
+            if (self.m_adjacency[VFrom][i] == 1) and (i == VTo):
+                path.append(i)
+                return path
+            if (self.m_adjacency[VFrom][i] == 1) and (self.vertex[i].Hit == False):
+                path.extend(self._depth_first_search(i, VTo))
+                if path[-1] == VTo:
+                    break
+        if path[-1] != VTo:
+            return []
+        return path
+
+
