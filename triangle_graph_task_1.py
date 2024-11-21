3f1c282a37d8eef1f31f117c80dbdd3391d8a794
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-11-21 23:42:31 +0500

    Update triangle_graph_task_1.py

--- triangle_graph_task_1.py
+++ triangle_graph_task_1.py
@@ -43,34 +43,32 @@ class SimpleGraph:
             self.m_adjacency[v1][v2] = 0
             self.m_adjacency[v2][v1] = 0
 
-    # 1.* Добавьте метод, подсчитывающий общее число треугольников в графе.
     def get_total_number_of_triangles(self):
         if self.vertex[0] is None:
-            return 0
+            return []
         visited_indices = [None] * self.max_vertex
-        return self._get_triangle_node(0, 0, visited_indices)
-        
-    def _get_triangle_node(self, index, n_triangles: int, visited_indices: list):
+        return self._get_total_number_of_triangles(0, visited_indices, 0)
+
+    def _get_total_number_of_triangles(self, index: int, visited_indices: list, n_triangles: int):
         if index == self.max_vertex:
             return n_triangles
         visited_indices[index] = 1
         if self.vertex[index] is None:
-            return self._get_triangle_node(index+1, n_triangles, visited_indices)
+            return self._get_total_number_of_triangles(index+1, visited_indices, n_triangles) 
         connected_nodes = []
         for i in range(self.max_vertex):
             if self.vertex[i] is None:
                 continue
-            if (self.m_adjacency[index][i] == 1) and (visited_indices[i] is not None):
+            if (self.m_adjacency[index][i] == 1) and (visited_indices[i] is None):
                 connected_nodes.append(i)
         if len(connected_nodes) <= 1:
-            return self._get_triangle_node(index+1, n_triangles, visited_indices)
-        # check connected nodes for triangle
+            return self._get_total_number_of_triangles(index+1, visited_indices, n_triangles)
         for j in range(len(connected_nodes)-1):
             for k in range(j, len(connected_nodes)):
                 v1 = connected_nodes[j]
                 v2 = connected_nodes[k]
-                if (self.m_adjacency[index][v1] == 1) and (self.m_adjacency[index][v2] == 1) and (self.m_adjacency[v1][v2] == 1):
+                if self.m_adjacency[v1][v2] == 1:
                     n_triangles += 1
-        return self._get_triangle_node(index+1, n_triangles, visited_indices)
+        return self._get_total_number_of_triangles(index+1, visited_indices, n_triangles)
 
 
