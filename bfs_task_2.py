a907786e12e16b6d7db66ff9349d90bb45d7b88e
Slava Morozov <110766489+morozov-slava@users.noreply.github.com>
2024-11-26 21:39:44 +0500

    Update bfs_task_2.py

--- bfs_task_2.py
+++ bfs_task_2.py
@@ -3,6 +3,7 @@ class Node:
         self.value = v
         self.next = None
 
+
 class Queue:
     def __init__(self):
         self.head = None
@@ -27,91 +28,76 @@ class Queue:
         return self.head is None
 
 
-
-class SimpleTreeNode:
-    def __init__(self, val, parent):
-        self.NodeValue = val
-        self.Parent = parent
-        self.Children = []
-	
-class SimpleTree:
-    def __init__(self, root):
-        self.Root = root
-
-    def _get_all_nodes(self, StartNode):
-        all_nodes = [StartNode]
-        for N in StartNode.Children:
-            if len(N.Children):
-                all_nodes.extend(self._get_all_nodes(N))
-            else:
-                all_nodes.append(N)
-        return all_nodes
-
-    def _get_node_by_value(self, StartNode, value):
-        nodes = []
-        if StartNode.NodeValue == value:
-            nodes.append(StartNode)
-        for N in StartNode.Children:
-            nodes.extend(self._get_node_by_value(N, value))
-        return nodes
-
-    def _get_number_of_nodes(self, StartNode):
-        n_nodes = 1
-        for N in StartNode.Children:
-            n_nodes += self._get_number_of_nodes(N)
-        return n_nodes
-
-    def _get_number_of_leaves(self, StartNode):
-        n_leaves = 0
-        if len(StartNode.Children) == 0:
-            n_leaves += 1
-            return n_leaves
-        for N in StartNode.Children:
-            n_leaves += self._get_number_of_leaves(N)
-        return n_leaves
-	
-    def AddChild(self, ParentNode, NewChild):
-        ParentNode.Children.append(NewChild)
-        NewChild.Parent = ParentNode
-  
-    def DeleteNode(self, NodeToDelete):
-        parent_node = NodeToDelete.Parent
-        parent_node.Children.remove(NodeToDelete)
-        NodeToDelete.Parent = None
+class Vertex:
+    def __init__(self, val):
+        self.Value = val
+        self.Hit = False
         
-    def GetAllNodes(self):
-        return self._get_all_nodes(self.Root)
+  
+class SimpleGraph:
+    def __init__(self, size: int):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex = [None] * size
+        
+    def AddVertex(self, v: int):
+        empty_index = None
+        for i in range(len(self.vertex)):
+            if self.vertex[i] is None:
+                empty_index = i
+                break
+        if empty_index is not None:
+            self.vertex[empty_index] = Vertex(v)
 
-    def FindNodesByValue(self, val):
-        return self._get_node_by_value(self.Root, val)
-   
-    def MoveNode(self, OriginalNode, NewParent):
-        self.DeleteNode(OriginalNode)
-        NewParent.Children.append(OriginalNode)
-        OriginalNode.Parent = NewParent
-   
-    def Count(self):
-        return self._get_number_of_nodes(self.Root)
-
-    def LeafCount(self):
-        return self._get_number_of_leaves(self.Root)
-
+    def RemoveVertex(self, v: int):
+        if self.vertex[v] is not None:
+            self.vertex[v] = None
+        # remove edges
+        for i in range(self.max_vertex):
+            if self.m_adjacency[v][i] != 0:
+                self.m_adjacency[v][i] = 0
+            if self.m_adjacency[i][v] != 0:
+                self.m_adjacency[i][v] = 0 
+	
+    def IsEdge(self, v1: int, v2: int):
+        if (self.m_adjacency[v1] is None) or (self.m_adjacency[v2] is None):
+            return False
+        return bool(self.m_adjacency[v1][v2]) and bool(self.m_adjacency[v2][v1])
+	
+    def AddEdge(self, v1: int, v2: int):
+        if (self.m_adjacency[v1] is not None) or (self.m_adjacency[v2] is not None):
+            self.m_adjacency[v1][v2] = 1
+            self.m_adjacency[v2][v1] = 1
+	
+    def RemoveEdge(self, v1: int, v2: int):
+        if (self.m_adjacency[v1] is not None) and (self.m_adjacency[v2] is not None):
+            self.m_adjacency[v1][v2] = 0
+            self.m_adjacency[v2][v1] = 0
+    
     # 2.* Используя BFS, найдите два наиболее удалённых друг от друга узла в обычном дереве, 
-    #     которое теперь воспринимаем как граф, и возвращайте это максимальное расстояние. 
+    #     которое теперь воспринимаем как граф, и возвращайте это максимальное расстояние.
     def FindMaxNodesDistance(self):
-        if self.Root is None:
+        if self.vertex[0] is None:
             return 0
-        queue = Queue()
-        queue.enqueue(Node((self.Root, 0)))
-        max_distance = 0
-        farthest_node = self.Root
-        while not queue.is_empty():
-            node, distance = queue.dequeue().value
-            for child in node.Children:
-                queue.enqueue(Node((child, distance+1)))
-                if distance + 1 > max_distance:
-                    max_distance = distance + 1
-                    farthest_node = child
+        first_farthest_node_index, _ = self._one_node_bfs(0)
+        _, max_distance = self._one_node_bfs(first_farthest_node_index)
         return max_distance
+    
+    def _one_node_bfs(self, start_node_index: int):
+        queue = Queue()
+        queue.enqueue(Node((start_node_index, 0)))
+        visited_indices = [None] * self.max_vertex
+        max_distance = 0
+        farthest_node_index = start_node_index
+        while not queue.is_empty():
+            index, distance = queue.dequeue().value
+            visited_indices[index] = 1
+            if distance > max_distance:
+                max_distance = distance
+                farthest_node_index = index
+            for i in range(self.max_vertex):
+                if self.m_adjacency[index][i] == 1 and visited_indices[i] is None:
+                    queue.enqueue(Node((i, distance+1)))
+        return farthest_node_index, max_distance
 
 
